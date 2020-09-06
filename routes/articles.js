a8fda03b0a3fd9e2e5cde0d703a23d4a15f3b4b2
aryan-tiwari <aryantiwari91000@gmail.com>
2020-09-06 21:36:41 +0530

    v0.1 markdown works completely

--- routes/articles.js
+++ routes/articles.js
@@ -6,8 +6,11 @@ router.get('/new', (req, res)=>{
     res.render('articles/new', {article: new Article()});
 });
 
-router.get('/:id', (req, res)=>{
-    res.send(req.params.id);
+router.get('/:id', async(req, res)=>{
+    const article = await Article.findById(req.params.id)
+    if(article == null) res.redirect('/')
+
+    res.render('articles/show', {article: article});
 })
 
 router.post('/', async(req, res)=>{
